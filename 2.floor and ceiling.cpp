7d0571041f4a596e2719086f1963c56b1f6accc7
Abishek Tamang <79093702+AbishekTamangGlan@users.noreply.github.com>
2021-04-11 11:30:19 +0545

    Add files via upload

---
+++ 2.floor and ceiling.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+	float a;
+	int i;
+	printf("Enter the floating value");
+	scanf("%f",&a);
+	printf("\nthe ceiling value is %f",ceil(a));
+	printf("\nthe floor value is %f",floor(a));
+	return 0;
+}
